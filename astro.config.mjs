ae2ad6c4670466b882620fa7af1fb823934b1c38
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-06-04 21:33:56 -0700

    stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,53 +1,18 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 import icon from "astro-icon";
-import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
-import { epoxyPath } from "@mercuryworkshop/epoxy-transport";
-import { bareModulePath } from "@mercuryworkshop/bare-as-module3";
-import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
 
 
 import netlify from "@astrojs/netlify";
 
 
-import partytown from "@astrojs/partytown";
-
-
 export default defineConfig({
-  integrations: [icon(), (await import("@playform/compress")).default(), partytown()],
+  integrations: [icon(), (await import("@playform/compress")).default()],
 
   vite: {
     plugins: [
       tailwindcss(),
-      viteStaticCopy({
-        targets: [
-          {
-            src: uvPath,
-            dest: "",
-            rename: "uv",
-            overwrite: false,
-          },
-          {
-            src: epoxyPath,
-            dest: "",
-            rename: "epoxy",
-            overwrite: false,
-          },
-          {
-            src: bareModulePath,
-            dest: "",
-            rename: "baremod",
-            overwrite: false,
-          },
-          {
-            src: baremuxPath,
-            dest: "",
-            rename: "baremux",
-          },
-        ],
-      }),
     ],
   },
 
--- public/uv.config.js
+++ public/uv.config.js
@@ -1,6 +1,6 @@
 
 self.__uv$config = {
-  prefix: "/~/uv/",
+  prefix: "/uv/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet/dist/uv.handler.js",
