ff9f55d42ff285e1da3057961db17b93c4160d19
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-06-17 16:21:56 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
-import icon from "astro-icon";
+import { defineConfig } from "astro/config"
+import tailwindcss from "@tailwindcss/vite"
+import icon from "astro-icon"
 
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from "@astrojs/cloudflare"
 
 export default defineConfig({
   integrations: [icon(), (await import("@playform/compress")).default()],
@@ -13,18 +13,16 @@ export default defineConfig({
       sourcemap: true, // enable production source maps
     },
     css: {
-      devSourcemap: true // enable CSS source maps during development
+      devSourcemap: true, // enable CSS source maps during development
     },
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
 
   output: "server",
   adapter: cloudflare({
-    imageService: 'cloudflare',
+    imageService: "cloudflare",
   }),
   experimental: {
     preserveScriptOrder: true,
   },
-});
+})
