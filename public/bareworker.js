01348628084bb05dcc15f4c45607b750e2a15931
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-04 21:05:43 -0700

    some stuff

--- public/bareworker.js
+++ public/bareworker.js
@@ -1,4 +1,4 @@
 // Used as a wrapper for the bare-mux worker
 importScripts(
-  "https://cdn.jsdelivr.net/npm/@mercuryworkshop/bare-mux/dist/worker.js",
+  "https://unpkg.com/@mercuryworkshop/bare-mux@2.1.7/dist/worker.js",
 )
--- public/ultraworker.js
+++ public/ultraworker.js
@@ -1,4 +1,6 @@
-importScripts("https://brominecdn.netlify.app/uv/uv.bundle.js")
+importScripts(
+  "https://unpkg.com/@titaniumnetwork-dev/ultraviolet@3.2.10/dist/uv.bundle.js",
+)
 importScripts("uv.config.js")
 importScripts(__uv$config.sw)
 importScripts("/scram/scramjet.shared.js", "/scram/scramjet.worker.js")
