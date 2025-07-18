641bf31c400532d0685f39f01c5e706b00341437
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-07-18 13:00:00 -0700

    chore: removed unused packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,29 +3,22 @@ import { defineConfig } from "astro/config"
 import tailwindcss from "@tailwindcss/vite"
 import icon from "astro-icon"
 
-import cloudflare from "@astrojs/cloudflare"
-
 import partytown from "@astrojs/partytown";
 
-// import netlify from "@astrojs/netlify";
 
 export default defineConfig({
   integrations: [icon(), (await import("@playform/compress")).default(), partytown()],
 
   vite: {
     build: {
-      sourcemap: true, // enable production source maps
+      sourcemap: true,
     },
     css: {
-      devSourcemap: true, // enable CSS source maps during development
+      devSourcemap: true, 
     },
     plugins: [tailwindcss()],
   },
 
-  // output: "static",
-  // adapter: cloudflare({
-  //   imageService: "compile",
-  // }),
   experimental: {
     preserveScriptOrder: true,
   },
